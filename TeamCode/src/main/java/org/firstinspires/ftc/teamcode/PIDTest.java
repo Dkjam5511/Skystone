f590f4b2f8a19fb427d142ff582694a4edb62002
Arjun Thattankandy <arjun.baiju@gmail.com>
2019-08-11 19:34:47 -0500

    PID Test Code. Unfinished. Changed 'u' to 'y' and 'r' to 'x'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTest.java
@@ -3,99 +3,97 @@ package org.firstinspires.ftc.teamcode;
 public class PIDTest{
 
     //Figure out how to make it all in PID, vector should be combined anyway
-    private double uMagnitude; //Not really Magnitude, can be negative. To be Psuedo Stick Input
-    private double uError;
-    private double rMagnitude; //Not really Magnitude, can be negative. To be Psuedo Stick Input
-    private double rError;
+    private double yMagnitude; //Not really Magnitude, can be negative. To be Psuedo Stick Input
+    private double yError;
+    private double xMagnitude; //Not really Magnitude, can be negative. To be Psuedo Stick Input
+    private double xError;
     private double[] vector;
     private boolean reached;
     private double P; private double I; private double D;
-    private double uLastError;
-    private double rLastError;
-    private double uLastI;
-    private double rLastI;
+    private double yLastError;
+    private double xLastError;
+    private double xLastI;
+    private double yLastI;
     private double lastTime;
-    private double uIntegrater;
-    private double rIntegrater;
+    private double yIntegrater;
+    private double xIntegrater;
 
     public PIDTest(double up, double right, double p, double i,  double d){
 
-        uMagnitude = 0;
-        rMagnitude = 0;
-        uError = up;
-        rError = right;
+        yMagnitude = 0;
+        xMagnitude = 0;
+        yError = up;
+        xError = right;
         vector = new double[2];
         reached = false;
         P = p; I = i; D = d;
-        uLastError = 0;
-        rLastError = 0;
-        uLastI = 0;
-        rLastI = 0;
+        yLastError = 0;
+        xLastError = 0;
+        yLastI = 0;
+        xLastI = 0;
         lastTime = System.currentTimeMillis();
-        uIntegrater = 0;
-        rIntegrater = 0;
+        yIntegrater = 0;
+        xIntegrater = 0;
     }
     public double[] getCurrentVector(){return vector;}
     public boolean getReached(){return reached;}
-    public double[] calcErrors(double u, double r) {
-        uError = u;
-        rError = r;
-        return calcMagnitude(uError, rError);
+    public double[] calcErrors(double y, double x) {
+        yError = y;
+        xError = x;
+        return calcMagnitude(yError, xError);
     }
-    private double[] calcMagnitude(double uE, double rE){
+    private double[] calcMagnitude(double yE, double xE){
         //calc with error to get speed, do this in loop with calc and putting in values.
         //Find a way to get reached = true when mags = 0, then can end loop
         //Copy in Drew's Mecanum calcs
 
         //Reset Values from last run-through
-        uMagnitude = 0;
-        uIntegrater = 0;
-        rMagnitude = 0;
-        rIntegrater = 0;
+        yMagnitude = 0;
+        yIntegrater = 0;
+        xMagnitude = 0;
+        xIntegrater = 0;
 
-        uMagnitude += uE * P;
-        uMagnitude += ((System.currentTimeMillis() - lastTime) * (uE - uLastError)) * D;
+        yMagnitude += yE * P;
+        yMagnitude += ((System.currentTimeMillis() - lastTime) * (yE - yLastError)) * D;
         //Not adding the integral just yet, need to check if windup occured
-        uIntegrater = ((System.currentTimeMillis() - lastTime) * (uE * uLastError/2) + uLastI) * I;
-        if (uMagnitude + uIntegrater > 1 && uE > 0 && uIntegrater > 0){
+        yIntegrater = ((System.currentTimeMillis() - lastTime) * (yE * yLastError/2) + yLastI) * I;
+        if (yMagnitude + yIntegrater > 1 && yE > 0 && yIntegrater > 0){
             //Integral is winding up in the positive direction, so it is getting clamped
-            uMagnitude = 1;
+            yMagnitude = 1;
         }
-        else if (uMagnitude + uIntegrater < -1 && uE < 0 && uIntegrater < 0){
+        else if (yMagnitude + yIntegrater < -1 && yE < 0 && yIntegrater < 0){
             //Integral is winding up in the negative direction, so it is getting clamped
-            uMagnitude = -1;
+            yMagnitude = -1;
         }
         else {
             //Integral is not winding up, so we can safely use the value and add it in!
-            uMagnitude += uIntegrater;
+            yMagnitude += yIntegrater;
         }
 
-        rMagnitude += rE * P;
-        rMagnitude += ((System.currentTimeMillis() - lastTime) * (rE - rLastError)) * D;
-        rIntegrater += ((System.currentTimeMillis() - lastTime) * (rE * rLastError/2) + rLastI) * I;
-        if (rMagnitude + rIntegrater > 1 && rE > 0 && rIntegrater > 0){
+        xMagnitude += xE * P;
+        xMagnitude += ((System.currentTimeMillis() - lastTime) * (xE - xLastError)) * D;
+        xIntegrater += ((System.currentTimeMillis() - lastTime) * (xE * xLastError/2) + xLastI) * I;
+        if (xMagnitude + xIntegrater > 1 && xE > 0 && xIntegrater > 0){
             //Integral is winding up in the positive direction, so it is getting clamped
-            rMagnitude = 1;
+            xMagnitude = 1;
         }
-        else if (rMagnitude + rIntegrater < -1 && rE < 0 && rIntegrater < 0){
+        else if (xMagnitude + xIntegrater < -1 && xE < 0 && xIntegrater < 0){
             //Integral is winding up in the negative direction, so it is getting clamped
-            rMagnitude = -1;
+            xMagnitude = -1;
         }
         else {
             //Integral is not winding up, so we can safely use the value and add it in!
-            rMagnitude += rIntegrater;
+            xMagnitude += xIntegrater;
         }
-        uLastI = uIntegrater;
-        rLastI = rIntegrater;
+        xLastI = xIntegrater;
+        xLastI = xIntegrater;
         lastTime = System.currentTimeMillis();
-        uLastError = uE;
-        rLastError = rE;
+        yLastError = yE;
+        xLastError = xE;
 
-        vector[0] = uMagnitude;
-        vector[1] = rMagnitude;
+        vector[0] = yMagnitude;
+        vector[1] = xMagnitude;
 
         return vector;
-
-
     }
 }
